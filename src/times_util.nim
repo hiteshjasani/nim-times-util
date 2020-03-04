55639c7cac723378f0c9a3fc1a9e6289b3d2ef13
Hitesh Jasani <hitesh.jasani@gmail.com>
2020-03-04 08:59:31 -0500

    Refactor api

--- src/times_util.nim
+++ src/times_util.nim
@@ -1,14 +1,36 @@
 import times
 
-proc midnight*(year: int, month: Month, dayOfMonth: MonthdayRange): DateTime =
+export times
+
+type
+  Style* = enum
+    stUs1 = "ddd MMM d, yyyy",               # Sun Mar 1, 2020
+    stUs2 = "ddd MMM d",                     # Sun Mar 1
+    stUs3 = "ddd MMM d, ''yy",               # Sun Mar 1 '20
+    stISO8601a = "yyyy-MM-dd' 'HH:mm:sszzz", # 2020-03-01 23:59:50-05:00
+    stISO8601b = "yyyy-MM-dd'T'HH:mm:sszzz", # 2020-03-01T23:59:50-05:00
+    stISO8601c = "yyyy-MM-dd"                # 2020-03-01
+
+
+proc day*(year: int, month: Month, dayOfMonth: MonthdayRange): DateTime =
   ## At midnight for specified day in local timezone
   initDateTime(dayOfMonth, month, year, 0, 0, 0)
 
-proc midnightToday*(): DateTime =
-  let n = now()
-  midnight(n.year, n.month, n.monthday)
+proc midnight*(year: int, month: Month, dayOfMonth: MonthdayRange): DateTime
+  {.deprecated: "use day instead".} =
+  ## At midnight for specified day in local timezone
+  day(year, month, dayOfMonth)
 
-proc asDayUS*(dt: DateTime, style: int): string =
+proc today*(): DateTime =
+  ## Today at midnight in the local timezone
+  let n = now()
+  day(n.year, n.month, n.monthday)
+
+proc midnightToday*(): DateTime {.deprecated: "use today instead".} =
+  today()
+
+proc asDayUS*(dt: DateTime, style: int): string
+  {.deprecated: "use fmt instead".} =
   case style
   of 1:
     format(dt, "ddd MMM d, YYYY")
@@ -18,3 +40,17 @@ proc asDayUS*(dt: DateTime, style: int): string =
     format(dt, "ddd MMM d, YYYY")
   else:
     "unsupported style"
+
+proc fmt*(dt: DateTime, style: Style): string =
+  ## Format the date/time as a string.
+  ##
+  ## Some styles are lossy, as in they lose time information and
+  ## therefore won't work in a roundtripping situation.
+  ##
+  ##   let t = initDatetime(31, mDec, 2019, 23, 59, 50)
+  ##   check t == fmt(t, stIso8601a).parse(stIso8601a)  # works
+  ##   check t == fmt(t, stUs1).parse(stUs1)            # fails
+  format(dt, $style)
+
+proc parse*(s: string, style: Style): DateTime =
+  parse(s, $style)
---
+++ tests/ttimes_util.nim
@@ -0,0 +1,59 @@
+# This is just an example to get you started. You may wish to put all of your
+# tests into a single file, or separate them into multiple `test1`, `test2`
+# etc. files (better names are recommended, just make sure the name starts with
+# the letter 't').
+#
+# To run these tests, simply execute `nimble test`.
+
+import unittest
+
+import times_util
+
+
+
+suite "Creating days":
+  test "arbitrary day":
+    check:
+      "2020-01-31 00:00:00-05:00" == fmt(day(2020, mJan, 31), stISO8601a)
+      "2019-12-31 00:00:00-05:00" == fmt(day(2019, mDec, 31), stISO8601a)
+
+  test "today":
+    let exp = today()
+    check(exp == fmt(now(), stISO8601c).parse(stISO8601c))
+
+
+suite "Roundtripping":
+  test "starting with ISO8601a":
+    let exp = "2020-01-31 00:00:00-05:00"
+    check exp == parse(exp, stISO8601a).fmt(stISO8601a)
+
+  test "starting with ISO8601b":
+    let exp = "2020-01-31T00:00:00-05:00"
+    check exp == parse(exp, stISO8601b).fmt(stISO8601b)
+
+  test "starting with ISO8601c":
+      let exp = "2020-01-31"
+      check exp == parse(exp, stISO8601c).fmt(stISO8601c)
+
+  test "starting with date/time":
+    let exp = initDateTime(31, mDec, 2019, 23, 59, 50)
+    check:
+      exp == fmt(exp, stISO8601a).parse(stISO8601a)
+      exp == fmt(exp, stISO8601b).parse(stISO8601b)
+
+  test "lossy formats don't work":
+    let exp = initDateTime(31, mDec, 2019, 23, 59, 50)
+    let fmts = @[stUs1, stUs2, stUs3, stISO8601c]
+    check:
+      exp != fmt(exp, stUs1).parse(stUs1)
+      exp != fmt(exp, stUs2).parse(stUs2)
+      exp != fmt(exp, stUs3).parse(stUs3)
+      exp != fmt(exp, stISO8601c).parse(stISO8601c)
+
+  # TODO: Decide which coding style of test we like better and remove
+  # one of the lossy format tests.
+  test "lossy formats don't work, v2":
+    let exp = initDateTime(31, mDec, 2019, 23, 59, 50)
+    let fmts = @[stUs1, stUs2, stUs3, stISO8601c]
+    for style in fmts:
+      check exp != fmt(exp, style).parse(style)
